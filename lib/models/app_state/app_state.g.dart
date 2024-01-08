32b44e7d31b147b322b3c7ee173d17c77d87bfee
Tosa Cezar <67374801+CZew2003@users.noreply.github.com>
2024-01-08 09:15:01 +0200

    changed style

--- lib/models/app_state/app_state.g.dart
+++ lib/models/app_state/app_state.g.dart
@@ -8,7 +8,9 @@ part of 'app_state.dart';
 
 _$AppState$Impl _$$AppState$ImplFromJson(Map<String, dynamic> json) => _$AppState$Impl(
       isLoading: json['isLoading'] as bool? ?? false,
-      images: (json['images'] as List<dynamic>?)?.map((e) => ImageModel.fromJson(e as Map<String, dynamic>)).toList() ??
+      images: (json['images'] as List<dynamic>?)
+              ?.map((dynamic e) => ImageModel.fromJson(e as Map<String, dynamic>))
+              .toList() ??
           const <ImageModel>[],
       page: json['page'] as int? ?? 1,
       lastQuery: json['lastQuery'] as String? ?? '',
